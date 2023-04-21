88370575e740951d2e55ce68c404fb83d1dae977
sachyshyn <alexandr.sachyshyn@gmail.com>
2023-04-21 13:53:47 +0200

    feat: added basic markup without styles and animated block

---
+++ src/pages/main.tsx
@@ -0,0 +1,26 @@
+export function MainPage() {
+  return (
+    <main>
+      <h1>Main Page</h1>
+      <p>This text has been generated on the client side</p>
+      <section>
+        <h2>The content below has been shuffled on the client side</h2>
+        <div>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+        </div>
+      </section>
+      <section>
+        <h2>the elements animation has been randomly chosen on the cliend side</h2>
+        <div></div>
+      </section>
+    </main>
+  );
+}
